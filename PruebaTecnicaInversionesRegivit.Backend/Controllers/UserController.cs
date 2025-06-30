a16815d6163394acdd998732fc7d34abd312f908
Gustavo Cano <gustavo.canou7@gmail.com>
2025-06-29 21:25:22 -0600

    Creating API of User Login

---
+++ PruebaTecnicaInversionesRegivit.Backend/Controllers/UserController.cs
@@ -0,0 +1,54 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Identity.Data;
+using Microsoft.AspNetCore.Mvc;
+using PruebaTecnicaInversionesRegivit.Backend.Data;
+using PruebaTecnicaInversionesRegivit.Shared.DTOs.User;
+using PruebaTecnicaInversionesRegivit.Shared.Models;
+
+namespace PruebaTecnicaInversionesRegivit.Backend.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class UserController : ControllerBase
+    {
+        private readonly AppDbContext _context;
+
+
+        public UserController(AppDbContext context)
+        {
+            _context = context;
+        }
+
+        [HttpPost("login")]
+        public IActionResult Login([FromBody] UserLoginDto request)
+        {
+            var user = _context.Users.FirstOrDefault(u =>
+                u.Username == request.Username &&
+                u.Password == request.Password);
+
+            if (user == null)
+            {
+                return Ok(new LoginResponse
+                {
+                    Success = false,
+                    Message = "Usuario o contraseña incorrectos"
+                });
+            }
+
+            // En producción, nunca devolver la contraseña
+            var userResponse = new UserGetDto
+            {
+                Id = user.Id,
+                Username = user.Username,
+                Role = user.Role
+            };
+
+            return Ok(new LoginResponse
+            {
+                Success = true,
+                Message = "Autenticación exitosa",
+                User = userResponse
+            });
+        }
+    }
+}
---
+++ PruebaTecnicaInversionesRegivit.Shared/DTOs/User/LoginResponse.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PruebaTecnicaInversionesRegivit.Shared.DTOs.User
+{
+    public class LoginResponse
+    {
+        public bool Success { get; set; }
+        public string Message { get; set; }
+        public UserGetDto User { get; set; }
+    }
+}
